6c7f44225219974cf9e947c8927f9521df46607a
dosealy <sealy.dong@gmail.com>
2016-08-28 11:54:49 +0800

    update

---
+++ assets/www/mapjs/api_v2.0.js
@@ -0,0 +1,8 @@
+(function() {
+    window.wise = 1;
+    window.netSpeed = 212;
+    window.netType = 1;
+    window.BMap_loadScriptTime = (new Date).getTime();
+    document.write('<script type="text/javascript" src="http://api.map.baidu.com/getscript?v=2.0&ak=lCdbRYbvM4PF10DLCxllpcFMej6A0GGn&services=&t=20160721145132"></script>');
+})();
+
