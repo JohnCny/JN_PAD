845800ee79dc93a67d6085b0e8757398810fff06
sealy <dongxl@qkjr.com.cn>
2016-12-08 14:44:13 +0800

    在线地图js离线化

--- assets/www/mapjs/api_v2.0.js
+++ assets/www/mapjs/api_v2.0.js
@@ -3,7 +3,7 @@
     window.netSpeed = 212;
     window.netType = 1;
     window.BMap_loadScriptTime = (new Date).getTime();
-    document.write('<script type="text/javascript" src="http://api.map.baidu.com/getscript?v=2.0&ak=lCdbRYbvM4PF10DLCxllpcFMej6A0GGn&services=&t=20160721145132"></script>');
+    document.write('<script type="text/javascript" src="mapjs/bmap.js"></script>');
 })();
 
 
